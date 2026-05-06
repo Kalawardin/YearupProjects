11514ad3276ab76d72d90f21a2d892f6ce8ee774
Kalawardin <baslar.fuat@gmail.com>
2026-05-06 12:30:53 -0500

    net worth Done

---
+++ week-05/Ex2A_MathScripts/net_worth.py
@@ -0,0 +1,7 @@
+total_assets = 500
+total_debts = 200
+net_worth = total_assets - total_debts
+
+print(f"Your total assets are {total_assets}")
+print(f"Your total assets are {total_debts}")
+print(f"Your total assets are {net_worth}")
