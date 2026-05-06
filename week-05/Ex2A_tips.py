ef90e1f78b1bb8c3116d751106e3bbf2c8f6b60e
Kalawardin <baslar.fuat@gmail.com>
2026-05-06 12:01:47 -0500

    2A_tips Done

---
+++ week-05/Ex2A_tips.py
@@ -0,0 +1,12 @@
+# Define known values
+food_cost = 79.25
+tax = 6.54
+tip = 12.00
+# Calculate the unknown
+total_due = food_cost + tax + tip
+# Display the results
+print("The total due is " + str(total_due)) # convert str gives us a permission for combining this two variable.
+
+print("Food cost is " + str(food_cost) + " and tax is " + str(tax))
+print("Tip is " + format(tip, ".2f"))
+print("Total due is " + str(total_due))
