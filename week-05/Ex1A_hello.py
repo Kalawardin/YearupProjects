79cd9c04660720b29a920504538b0e98593b91c7
Kalawardin <baslar.fuat@gmail.com>
2026-05-06 11:06:05 -0500

    1A Done

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,14 @@
 print("Hello world!")
+
+message = "Hello world!"
+print(message)
+
+# "Hello world" prints twice because first one printed a string literal but second one printed message variable.
+
+
+# Displaying dollars and cents
+dollars = 3
+cents = .50
+cents = cents + .25
+print(dollars+cents)
+# We can do mathematical calculations between strings and floats
