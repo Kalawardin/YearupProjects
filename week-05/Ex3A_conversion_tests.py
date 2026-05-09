fbc86cb44e5962aa7eae1a7ee34b47090f99a9f2
Kalawardin <baslar.fuat@gmail.com>
2026-05-09 13:33:27 -0500

    3A Done

---
+++ week-05/Ex3A_conversion_tests.py
@@ -0,0 +1,13 @@
+# Description: This script tests various numeric
+# conversion techniques
+# Author: Sam Q. Newprogrammer
+
+a = " 101.1 "
+b = int('55')
+c = "402 Stevens"
+d = 'Number 5 '
+
+print(a.strip(),type(a))
+print(b,type(b))
+print(c,type(c))
+print(d.strip(),type(d))
---
+++ week-05/Ex3A_string_cleaning.py
@@ -0,0 +1,12 @@
+name_1 = "PRIYA SHARMA"
+name_2 = "bob NGUYEN"
+name_3 = "LaTonya Williams"
+salary_1 = "$82,500"
+salary_2 = "$74,000"
+
+print(name_1.lower().title())
+print(name_2.lower().title())
+print(name_3.lower().title())
+print(salary_1.lower().replace("$",""))
+print(type(salary_1))
+print(salary_2.lower().replace("$",""))
